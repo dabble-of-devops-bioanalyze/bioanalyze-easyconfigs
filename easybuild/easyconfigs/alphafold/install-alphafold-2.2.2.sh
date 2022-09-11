452771d9b927f73b1ee7b7229080fe569c34d455
Jillian Rowe <jillian.e.rowe@gmail.com>
2022-09-11 18:07:22 +0300

    commit

--- easybuild/easyconfigs/alphafold/install-alphafold-2.2.2.sh
+++ easybuild/easyconfigs/alphafold/install-alphafold-2.2.2.sh
@@ -12,7 +12,7 @@ tar -xvf v2.2.2.tar.gz
 
 cd alphafold-2.2.2
 ${INSTALLDIR}/bin/pip3 install -r ./alphafold-2.2.2-requirements.txt
-${INSTALLDIR}/bin/pip3 install -r ./requirements.txt
+#${INSTALLDIR}/bin/pip3 install -r ./requirements.txt
 
 ${INSTALLDIR}/bin/pip3 install --upgrade \
       jax==0.2.14 \
