5c5474d1707226a24997ef636168310f6d7adb64
Jillian Rowe <jillian.e.rowe@gmail.com>
2022-10-30 11:51:12 +0300

    commit

--- easybuild/easyconfigs/pyautofep/install-pyautofep-2022.10.20.sh
+++ easybuild/easyconfigs/pyautofep/install-pyautofep-2022.10.20.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 export PATH=${INSTALLDIR}/bin:$PATH
 cd /tmp 
 wget https://github.com/luancarvalhomartins/PyAutoFEP/archive/9de6a9f93214ece37e5d8808a716234b4c0960d1.zip
@@ -8,4 +7,3 @@ unzip 9de6a9f93214ece37e5d8808a716234b4c0960d1.zip
 cd PyAutoFEP-9de6a9f93214ece37e5d8808a716234b4c0960d1/
 
 cp -rf * ${INSTALLDIR}/bin/
-
